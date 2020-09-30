61d08481d27cd30bc842ca74b12e08f39b17e2fd
Katelinn Jones <katiejonesyo@gmail.com>
2020-09-29 19:01:33 -0700

    style changes, buttons, start of personalization

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
 // import functions and grab DOM elements
+import { compareNumbers } from './compare-numbers.js';
+
+const guessButton = document.getElementById('guess-button');
+const userGuess = document.getElementById('number-input');
+const results = document.getElementById('results');
+const resetButton = document.getElementById('reset-button');
 
 // initialize state
+let totalGuesses = 4;
+let correctNumber = Math.floor(Math.random() * 15);
 
 // set event listeners to update state and DOM
+guessButton.addEventListener('click', () => {
+    totalGuesses--;
+
+    const guess = userGuess.valueAsNumber;
+    const comparedResult = compareNumbers(guess, correctNumber);
+
+    // LOSE
+    if (totalGuesses === 0) {
+        document.getElementById("guess-button").disabled = true;
+        return results.textContent = 'You are out of guesses! You lose!';
+    }
+
+    //TOO HIGH
+    if (comparedResult === 1) {
+        return results.textContent = `Too high! Guesses remaining: ${totalGuesses}`;
+    }
+
+    //TOO LOW
+    if (comparedResult === -1) {
+        return results.textContent = `Too low! Guesses remaining: ${totalGuesses}`;
+    }
+
+    // WIN
+    if (comparedResult === 0) {
+        document.getElementById("guess-button").disabled = true;
+        return results.textContent = 'You win! Congrats!';
+    }
+
+    document.getElementById('number-input').value = '';
+});
+
+// RESET
+resetButton.addEventListener('click', () => {
+    document.location.reload();
+});
---
+++ test/utils.js
@@ -0,0 +1,45 @@
+// import { example } from '../example.js';
+import { compareNumbers } from '../compare-numbers.js';
+
+const test = QUnit.test;
+
+test('compare-numbers should take in two same numbers and return 0', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+  
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('compare-numbers should take in two numbers and return 1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+  
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('compare-numbers should take in two numbers and return -1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
